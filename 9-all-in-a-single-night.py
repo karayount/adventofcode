3a1b0757cd01c36880105eff9372ecdcb5524212
Kara Yount <kara.m.yount@gmail.com>
2016-01-17 16:16:00 -0800

    day 9 part 1 complete

---
+++ 9-all-in-a-single-night.py
@@ -0,0 +1,30 @@
+import itertools
+
+routes = {}
+cities = set([])
+
+with open("9-all-in-a-single-night.txt") as visits:
+    for line in visits:
+        segment = line.rstrip().split(" ")
+        start_city = segment[0]
+        end_city = segment[2]
+        segment_length = int(segment[4])
+        routes[(start_city, end_city)] = segment_length
+        routes[(end_city, start_city)] = segment_length
+        cities.add(start_city)
+        cities.add(end_city)
+
+cities = list(cities)
+
+short_route_length = 999999999
+for possible_route in itertools.permutations(cities):
+    current_route_length = 0
+    for index in range(len(possible_route)-1):
+        if (possible_route[index], possible_route[index+1]) in routes:
+            current_route_length += routes[(possible_route[index], possible_route[index+1])]
+        else:
+            print "This route isn't in your dictionary"
+    if current_route_length < short_route_length:
+        short_route_length = current_route_length
+
+print short_route_length
