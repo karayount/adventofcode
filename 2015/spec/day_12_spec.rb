22d341452f923522e559e6970b0d55c8e7dd2c8c
User One <excavator@Users-MacBook-Air.local>
2020-12-05 08:43:05 -0800

    Input dir

--- 2015/spec/day_12_spec.rb
+++ 2015/spec/day_12_spec.rb
@@ -4,24 +4,24 @@ describe 'Day12' do
   describe '#sum' do
     it 'example' do
       day_12 = Day12.new()
-      expect(day_12.sum('./day_12_example.txt')).to eql(45)
+      expect(day_12.sum('./input/day_12_example.txt')).to eql(45)
     end
 
     it 'day 12 part 1' do
       day_12 = Day12.new()
-      expect(day_12.sum('./day_12_input.txt')).to eql(156366)
+      expect(day_12.sum('./input/day_12_input.txt')).to eql(156366)
     end
   end
 
   describe '#ignore_red' do
     it 'example' do
       day_12 = Day12.new()
-      expect(day_12.ignore_red('./day_12_example.txt')).to eql(28)
+      expect(day_12.ignore_red('./input/day_12_example.txt')).to eql(28)
     end
 
     it 'day 12 part 2' do
       day_12 = Day12.new()
-      expect(day_12.ignore_red('./day_12_input.txt')).to eql(96852)
+      expect(day_12.ignore_red('./input/day_12_input.txt')).to eql(96852)
     end
   end
 end
